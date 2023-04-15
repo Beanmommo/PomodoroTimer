993199175134a510973a2e44b6e42ebf52b87ef2
Leonardo <leonardo.prasetyo5@gmail.com>
2023-04-15 21:07:03 +1000

    Pomodoro timer implemented but no button yet

--- src/components/PomodoroTimer.tsx
+++ src/components/PomodoroTimer.tsx
@@ -1,8 +1,35 @@
+import React, { useState } from 'react';
+
 
 function PomodoroTimer() {
-    const timer = "25:00"
+
+    let pomodoro= "25:00";
+    let time = 25*60; // 25mins  
+    const [timer_display, setDisplay] = useState(pomodoro)
+    
+
+    const startTimer = () => {
+        console.log('CLick')
+        setInterval(updateTimer, 1000)
+    }
+
+    const updateTimer = () => {
+        let minutes = Math.floor(time/60)
+        let seconds = time % 60
+        setDisplay(`${minutes}:${seconds}`)
+        console.log(timer_display)
+        //document.getElementById("timer-display")?.textContent = timer_display
+        if (time == 0) {
+            alert("times up")
+            clearInterval
+        }else{
+            time--
+        }
+
+    }
     return (
-        <h1>{timer}</h1>
-    ) 
+        <h1 onClick={startTimer} id="timer-display">{timer_display}</h1>
+    ); 
+
 }
 export default PomodoroTimer
